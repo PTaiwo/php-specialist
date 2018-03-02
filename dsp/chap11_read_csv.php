4b3f3a76d26e04d1b7a487da014da2fade5d951c
PTaiwo <33854291+PTaiwo@users.noreply.github.com>
2018-03-02 01:35:18 -0500

    Final Leg
    Closing out the chapters

--- dsp/chap11_read_csv.php
+++ dsp/chap11_read_csv.php
@@ -2,20 +2,29 @@
     
         $myfile = fopen("studentData.csv", "r") or die("Unable to open file");
         print("<table border=1>");
+        $total = 0;
+        $count = 0;
         while(($data = fgetcsv($myfile, 1000, ","))!== FALSE){
             print("<tr>");
+            
             for($x =0; $x < count($data); $x++)
             {
                 print("<td>");
                 print($data[$x]);
                 print("</td>");
             }
-            print("</tr>");                        
+            print("</tr>");  
+            $total += $data[4];
+            $count = count($data) + 1;
+            $avgscore = $total / $count;
             
         }
             
         print("</table>");        
         fclose($myfile);
         $_POST['name']= null;
+    
+        print("<br/>");
+        print("The average score is " . $avgscore);
     
 ?>
---
+++ dsp/retrieveCookie.php
@@ -0,0 +1,19 @@
+<?php
+    $cookieName = "userID";
+    if(!isset($_COOKIE[$cookieName]))
+    {
+        $out = "No User ID set";
+    } else
+    {
+        $out = $_COOKIE[$cookieName];
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Setting Cookies</title>
+    </head>
+    <body>
+        <p>Welcome: <?php print($out) ?></p>
+    </body>
+</html>
